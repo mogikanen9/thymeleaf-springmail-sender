fa0a076acb9b2dfb22f93a79abc347952066d5a2
mogikanen9 <voinov9@gmail.com>
2017-01-18 09:59:04 -0500

    First workable sample
    - Introduced service layer
    - Updated JavaMailSender config to rely on Spring Boot default params
    - Simplified html template is used

--- boot-email-app/src/main/java/com/mogikanensoftware/app/controller/MailSenderController.java
+++ boot-email-app/src/main/java/com/mogikanensoftware/app/controller/MailSenderController.java
@@ -1,62 +1,36 @@
 package com.mogikanensoftware.app.controller;
 
-import java.util.Arrays;
-import java.util.Date;
-
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.Context;
+
+import com.mogikanensoftware.app.service.EmailService;
+import com.mogikanensoftware.app.service.EmailServiceException;
 
 @RestController
 @RequestMapping("emails")
 public class MailSenderController {
 
-	private JavaMailSender mailSender;
-	private TemplateEngine templateEngine;
-
+	private EmailService emailService;
+	
 	@Autowired
-	@Qualifier("templateEngine")
-	public void setTemplateEngine(TemplateEngine templateEngine) {
-		this.templateEngine = templateEngine;
-	}
-
-	@Autowired
-	@Qualifier("mailSender")
-	public void setMailSender(JavaMailSender mailSender) {
-		this.mailSender = mailSender;
+	public void setEmailService(EmailService emailService){
+		this.emailService = emailService;
 	}
 
 	@RequestMapping(method = RequestMethod.GET, path = "/send")
 	public String sendHelloEmail(@RequestParam("firstName") String firstName, @RequestParam("lastName") String lastName)
-			throws MessagingException {
-
-		final Context ctx = new Context();
-		ctx.setVariable("firstName", firstName);
-		ctx.setVariable("lastName", firstName);
-		ctx.setVariable("subscriptionDate", new Date());
-		ctx.setVariable("hobbies", Arrays.asList("Cinema", "Sports", "Music"));
-
-		final String htmlContent = this.templateEngine.process("welcome.html", ctx);
-
-		final MimeMessage mimeMessage = this.mailSender.createMimeMessage();
-		final MimeMessageHelper message = new MimeMessageHelper(mimeMessage, "UTF-8");
-		message.setFrom("sender@example.com");
-		message.setTo("recipient@example.com");
-		message.setSubject("This is the message subject");
-		message.setText(htmlContent);
-		this.mailSender.send(mimeMessage);
-
+			throws EmailServiceException {
+		Map<String,String> params = new HashMap<>();
+		params.put("firstName", firstName);
+		params.put("lastName", lastName);
+		emailService.sendEmail("me@wer.com", "joe@qwe.com", "welcomeTemplate", "Hello from me", params);
 		return HttpStatus.ACCEPTED.toString();
 	}
 
