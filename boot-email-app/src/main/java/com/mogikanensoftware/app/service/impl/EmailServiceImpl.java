ff9efe22449acc7d66cd300726f9464f2c6abc61
mogikanen9 <voinov9@gmail.com>
2017-01-23 14:46:19 -0500

    Full unit test coverage for EmailServiceImpl
    PowerMock was introduced to mock final method call from ThymeLeaf
    classes

--- boot-email-app/src/main/java/com/mogikanensoftware/app/service/impl/EmailServiceImpl.java
+++ boot-email-app/src/main/java/com/mogikanensoftware/app/service/impl/EmailServiceImpl.java
@@ -21,16 +21,20 @@ public class EmailServiceImpl implements EmailService {
 
 	private JavaMailSender mailSender;
 	private TemplateEngine templateEngine;
+	private MimeMessageHelperCreator mimeMessageHelperCreator;
 
 
 	@Autowired
-	public EmailServiceImpl(@Qualifier("templateEngine")TemplateEngine templateEngine, @Qualifier("mailSender")JavaMailSender mailSender){
+	public EmailServiceImpl(@Qualifier("templateEngine")TemplateEngine templateEngine, 
+			@Qualifier("mailSender")JavaMailSender mailSender,
+			@Qualifier("mimeMessageHelperCreator") MimeMessageHelperCreator mimeMessageHelperCreator){
 		this.mailSender = mailSender;
 		this.templateEngine = templateEngine;
+		this.mimeMessageHelperCreator = mimeMessageHelperCreator;
 	}
 	
 	@Override
-	public void sendEmail(String from, String to, String templateName, String subject, Map<String, String> params)
+	public void sendEmail(final String from, final String to, final String templateName, final String subject, final Map<String, String> params)
 			throws EmailServiceException {
 		try {
 			final Context ctx = new Context();
@@ -40,7 +44,7 @@ public class EmailServiceImpl implements EmailService {
 			final String htmlContent = this.templateEngine.process(templateName, ctx);
 
 			final MimeMessage mimeMessage = this.mailSender.createMimeMessage();
-			final MimeMessageHelper message = new MimeMessageHelper(mimeMessage, "UTF-8");
+			final MimeMessageHelper message = mimeMessageHelperCreator.create(mimeMessage, "UTF-8");
 
 			message.setFrom(from);
 			message.setTo(to);
@@ -52,5 +56,6 @@ public class EmailServiceImpl implements EmailService {
 		}
 
 	}
+		
 
 }
--- boot-email-app/src/test/java/com/mogikanensoftware/app/service/impl/EmailServiceImplTest.java
+++ boot-email-app/src/test/java/com/mogikanensoftware/app/service/impl/EmailServiceImplTest.java
@@ -3,36 +3,44 @@ package com.mogikanensoftware.app.service.impl;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.mail.MessagingException;
+
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.spring4.SpringTemplateEngine;
 import org.thymeleaf.templateresolver.ITemplateResolver;
 
-import com.mogikanensoftware.app.service.EmailService;
 import com.mogikanensoftware.app.service.EmailServiceException;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(TemplateEngine.class)
 public class EmailServiceImplTest {
 
-	private EmailService emailService;
-
-	@Mock
-	JavaMailSender mailSender;
+	private EmailServiceImpl emailService;	
 
 	@Before
 	public void setUp() throws Exception {
-		TemplateEngine templateEngine = new SpringTemplateEngine();
-		ITemplateResolver templateResolver = Mockito.mock(ITemplateResolver.class);
+		JavaMailSender mailSender = Mockito.mock(JavaMailSender.class);
+		TemplateEngine templateEngine = PowerMockito.mock(TemplateEngine.class);
+		ITemplateResolver templateResolver = PowerMockito.mock(ITemplateResolver.class);
+		MimeMessageHelperCreator mimeMessageHelperCreator = PowerMockito.mock(MimeMessageHelperCreator.class);
+		
+		MimeMessageHelper mimeMessageHelper = PowerMockito.mock(MimeMessageHelper.class);
+		Mockito.doThrow(new MessagingException("Invalid from")).when(mimeMessageHelper).setFrom("gavno@qwe.com");
+		PowerMockito.when(mimeMessageHelperCreator.create(Mockito.any(), Mockito.any())).thenReturn(mimeMessageHelper);
+		
 		templateEngine.setTemplateResolver(templateResolver);
-		emailService = new EmailServiceImpl(templateEngine, mailSender);
-		//Mockito.when(templateEngine.process(Mockito.anyString(), Mockito.any())).thenReturn("email html content");
+		emailService = new EmailServiceImpl(templateEngine, mailSender,mimeMessageHelperCreator);						
+		PowerMockito.when(templateEngine.process(Mockito.anyString(), Mockito.any())).thenReturn("email html content");
 	}
 
 	@After
@@ -44,9 +52,19 @@ public class EmailServiceImplTest {
 	public void testSendEmail() throws EmailServiceException {
 		Map<String, String> params = new HashMap<>();
 		params.put("firstName", "Jina");
-		params.put("lastName", "Doe");
-		//TODO - fix test
-		//emailService.sendEmail("abc@wer.com", "qwe@q.com", "welcomeTemplate", "hi", params);
+		params.put("lastName", "Doe");		
+		emailService.sendEmail("abc@wer.com", "qwe@q.com", "welcomeTemplate",
+		 "hi", params);
+	}
+	
+	@Test(expected = EmailServiceException.class)	
+	public void testSendEmailWithMimeException() throws MessagingException, EmailServiceException {
+		Map<String, String> params = new HashMap<>();
+		params.put("firstName", "Jina");
+		params.put("lastName", "Doe");		
+		emailService.sendEmail("gavno@qwe.com", "qwe@q.com", "welcomeTemplate",
+		 "hi", params);
+		Assert.fail();
 	}
 
 }
